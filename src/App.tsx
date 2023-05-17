a77b776de503c4afed570f328cd4fd5358e4f5cb
syssys131 <syssys131@gmail.com>
2023-05-17 16:08:43 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "./assets/styles/globalStyle";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>The First Step</h1>
+    </>
+  );
+};
+
+export default App;
